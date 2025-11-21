83ebc6425591b5b26090490cfb632d2ca303965d
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2025-11-21 16:57:58 +0530

    Number and Math Problems

---
+++ Lesson 01/1a.js
@@ -0,0 +1,33 @@
+//At restaurant, you order 1 soup for $10, 3 burgers for $8, and 1 ice cream for $5. calculate the order.
+
+10 + (3*8) + 5
+
+//you are at restaurant with 2 friends. calculate for above question. how much each should pay.
+
+(10 + (3*8) + 5) / 3
+
+// Calculate the total cost of the toaster($18.50) and 2 shirts ($7.50 each).
+
+//Convert dollars to cent then calculate and finally convert them back to dollars.
+(1850 + 750*2) / 100
+
+//Calculate a 10% tax for the total in above exercise.
+
+((1850 + 750*2)*0.1) / 100
+
+//Calculate a 20% tax for the total.
+
+((1850 + 750*2)*0.2) / 100
+
+//Calculate a basketball($20.95), shirts($7.99) and toasters($18.99)and shipping for toaster ($4.99).
+
+(2095 + 799 + 1899 + 499) / 100 //total before tax.
+
+//Calculate total tax 10%.
+(2095 + 799 + 1899 + 499) / 100 + Math.round((2095 + 799 + 1899 + 499) * 0.1) / 100
+
+//Roundoff 2.8=> 2.
+Math.floor(2.8)
+
+//round a numberup 2.2 => 3.
+Math.ceil(2.2)
---
+++ Lesson 01/1b.js
@@ -0,0 +1,13 @@
+/*Fahrenheit= (Celsius*9/5) + 32
+Celsius = (Fahrenheit -32)*5/9 */
+
+//celsius = 25 Fahrenheit = ?
+
+F = (25*(9/5)) + 32
+
+//Fahrenheit = 86, celsius = ?
+
+C = (86 - 32) * (5/9)
+
+//celsius = - 5, Fahrenheit = ?
+ F = (-5 * (9/5)) + 32
