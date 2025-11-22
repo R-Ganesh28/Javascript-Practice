397af392f5f3709754c75bdce2d2ed1a8598186e
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2025-11-22 22:27:10 +0530

    Day 03 - String

--- Lesson 02/2b.js
+++ Lesson 02/2b.js
@@ -1,4 +1,5 @@
 //In the Amazon, 2 basketballs ($20.95 each) with $4.99 shipping, and 2 shirts ($7.99 each) with $4.99 shipping.
+//Convert Dollars to cents for accurate calculations then revert it at last.
 
 //using interpolation, create the first line of text (use math to calculate the numbers)
 `Items (${2 + 2}): $${((2 * 2095) + (2 * 799)) / 100}`
@@ -11,4 +12,5 @@
 
 //create fourth line: 'Estimated tax (10%): $6.79'.
 'Estimated tax (' + 10 + '%): $' + (Math.round(((2 * 2095) + 499 + (2 * 799) + 499) * 0.1) / 100)
+
 
