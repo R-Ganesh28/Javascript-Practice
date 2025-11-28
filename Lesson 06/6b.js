9174173a2cff23e2662745b76966371c0add2368
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2025-11-28 17:19:08 +0530

    Converting a string Lowercase, uppercase & repeat

---
+++ Lesson 06/6b.js
@@ -0,0 +1,12 @@
+/* How to convert a string to all lowercase with JavaScript('GoodMorning' => 'good morning').
+*/
+
+console.log('Good Morning!'.toLowerCase());
+console.log('Good Evening!'.toUpperCase());
+
+/*
+How to repeat a string many times ('test' 2 times => testtest)
+*/
+
+console.log('Test'.repeat(2));
+
