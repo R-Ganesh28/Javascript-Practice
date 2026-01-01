c3d1bab0f6574c0936723becaeb3a443b4650978
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2026-01-01 21:22:36 +0530

    DOM (Document Object Manipulation)

---
+++ Lesson 07/Mini-Project/Rock Paper Scissors 4.js
@@ -0,0 +1,90 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+updateScoreElement();
+
+
+function playGame(playerMove) {
+
+    const computerMove = pickComputerMove();
+    let result = '';
+
+    if (playerMove === 'Scissors') {
+
+        if (computerMove === 'Rock') {
+            result = 'You lose.';
+        } else if (computerMove === 'Paper') {
+            result = 'You win.';
+        } else if (computerMove === 'Scissors') {
+            result = 'Tie.';
+        }
+
+    } else if (playerMove === 'Paper') {
+
+        if (computerMove === 'Rock') {
+            result = 'You win.';
+        } else if (computerMove === 'Paper') {
+            result = 'Tie.';
+        } else if (computerMove === 'Scissors') {
+            result = 'You lose.';
+        }
+
+    } else {
+        
+        if (computerMove === 'Rock') {
+            result = 'Tie.';
+        } else if (computerMove === 'Paper') {
+            result = 'You lose.';
+        } else if (computerMove === 'Scissors') {
+            result = 'You win.';
+        }
+
+    }
+
+    if (result === 'You win.') {
+        score.wins += 1;
+    } else if (result === 'You lose.') {
+        score.losses += 1;
+    } else if (result === 'Tie.') {
+        score.ties += 1;
+    }
+    
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement();
+
+    document.querySelector(".js-result").innerHTML = `${result}`;
+
+    document.querySelector(".js-moves").innerHTML = `You ${playerMove} - ${computerMove} Computer`;
+
+    /* 
+    alert(`You chose ${playerMove}. Computer chose ${computerMove}. ${result}
+Wins: ${score.wins}, Losses: ${score.losses}, Tie: ${score.ties}`);
+    */
+}
+
+function updateScoreElement() {
+    document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Tie: ${score.ties}`;
+}
+
+
+function pickComputerMove() {
+
+    const randomNumber = Math.random();
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1/3) {
+        computerMove = 'Rock';
+    } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+        computerMove = 'Paper';
+    } else if (randomNumber >= 2/3 && randomNumber < 1) {
+        computerMove = 'Scissors'
+    }
+
+    return computerMove;
+
+}
+
