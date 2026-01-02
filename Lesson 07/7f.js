77b100695481fe0719b27536d46a2a9fcb8848df
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2026-01-02 22:31:53 +0530

    Text Box with submit to display - DOM

---
+++ Lesson 07/7f.js
@@ -0,0 +1,4 @@
+function buttonElement() {
+    const inputElement = document.querySelector('.js-input');
+    document.querySelector('.js-result').innerHTML = `Your name is: ${inputElement.value}`;
+}
