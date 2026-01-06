d41a81c36bd6cdc1cd7d52ef6feb41a3e5fda5c8
Ganesh. R <130210233+R-Ganesh28@users.noreply.github.com>
2026-01-06 23:48:14 +0530

    YouTube Subscribe Button improved with CSS

---
+++ Lesson 08/8a.js
@@ -0,0 +1,11 @@
+function updateButton() {
+    const buttonElement = document.querySelector('.js-subscribe-button');
+
+    if (buttonElement.innerHTML === "Subscribe") {
+        buttonElement.innerHTML = 'Subscribed';
+        buttonElement.classList.add('is-subscribed');
+    } else {
+        buttonElement.innerHTML = 'Subscribe';
+        buttonElement.classList.remove('is-subscribed');
+    }
+}
